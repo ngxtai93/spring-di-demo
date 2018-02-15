959d027da4ce3fab9123aeda67fbbf3d0fdb6ee5
Tai Nguyen <ng.xtai93@gmail.com>
2018-02-14 20:46:38 -0600

    Create controller

--- src/main/java/tai/didemo/DiDemoApplication.java
+++ src/main/java/tai/didemo/DiDemoApplication.java
@@ -2,11 +2,16 @@ package tai.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import tai.didemo.controller.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+		MyController controller = (MyController) context.getBean("myController");
+
+		controller.hello();
 	}
 }
