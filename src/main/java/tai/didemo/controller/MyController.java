9147c9dbaeddc7762cbf1637893f724b5cfc7ac5
Tai Nguyen <ng.xtai93@gmail.com>
2018-02-14 20:46:54 -0600

    Create controller

---
+++ src/main/java/tai/didemo/controller/MyController.java
@@ -0,0 +1,13 @@
+package tai.didemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!");
+
+        return "foo";
+    }
+}
