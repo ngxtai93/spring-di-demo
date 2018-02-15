a73af1170ada1edaee23f5b06bdd776b0653da6b
Tai Nguyen <ng.xtai93@gmail.com>
2018-02-15 17:25:14 -0600

    Add new specifics Service

---
+++ src/main/java/tai/didemo/services/ConstructorGreetingService.java
@@ -0,0 +1,11 @@
+package tai.didemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello from Constructor Injected!";
+    }
+}
