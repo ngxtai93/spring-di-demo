73f32a8f3841d18d12b7bcebed03ddb8e56dcf8c
Tai Nguyen <ng.xtai93@gmail.com>
2018-02-17 16:35:06 -0600

    Refactor to use Repository

--- src/main/java/tai/didemo/services/PrimaryGreetingService.java
+++ src/main/java/tai/didemo/services/PrimaryGreetingService.java
@@ -8,8 +8,15 @@ import org.springframework.stereotype.Service;
 @Primary
 @Profile({"en", "default"})
 public class PrimaryGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hello from Primary Greeting Service!";
+        return greetingRepository.getEnglishGreeting();
     }
 }
